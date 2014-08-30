d137708dbb3fd842c78026263edacdc1ca417e29
Adam Streck <adam.streck@gmail.com>
2014-08-30 18:32:01 +0200

    No longer colliding with the sprite.

--- Assets/CreateLine.cs
+++ Assets/CreateLine.cs
@@ -7,27 +7,36 @@ public class CreateLine : MonoBehaviour
 	private Object line;
 	private Camera camera;
 	private float line_height = 1f;
+	private float sphere_radius = 3.75f;
 
 	// Use this for initialization
 	void Start () {
 		line = Resources.Load ("line");
 		camera = GameObject.Find ("Camera").GetComponent<Camera> ();
 	}
+
+	bool doesNotOverlap(Vector3 new_position, float lenght) {
+		float distance = Vector3.Distance (transform.position, last_point);
+		distance -= lenght / 2f;
+		return (sphere_radius < distance);
+	}
 	
-		// Update is called once per frame
+	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetMouseButton (0)) {
 			Vector3 new_point = camera.ScreenToWorldPoint (Input.mousePosition);
+			new_point.z = 0f;
 			if (last_point != Vector3.zero) {
-				new_point.z = 0f;
-
 				float distance = Vector3.Distance (new_point, last_point);
-				Vector3 instantiation = last_point + ((new_point - last_point) / 2f);
-				GameObject new_line = (GameObject)Instantiate (line, instantiation, Quaternion.FromToRotation(Vector3.right, last_point - new_point));
-				Vector3 new_scale = new_line.transform.localScale * distance;
-				new_scale.y = line_height;
-				new_line.transform.localScale = new_scale;
-				Debug.Log ("x: " + new_point.x + ", y: " + new_point.y);		
+				if(doesNotOverlap(new_point, distance)) {
+					Vector3 instantiation = last_point + ((new_point - last_point) / 2f);
+					GameObject new_line = (GameObject)Instantiate (line, instantiation, Quaternion.FromToRotation(Vector3.right, last_point - new_point));
+					// Scale
+					Vector3 new_scale = new_line.transform.localScale * distance;
+					new_scale.y = line_height;
+					new_line.transform.localScale = new_scale;
+					// Debug.Log ("x: " + new_point.x + ", y: " + new_point.y);	
+				}
 			}
 			last_point = new_point;
 			last_point.z = 0f;
--- Assets/StabilizeSprite.cs
+++ Assets/StabilizeSprite.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StabilizeCamera : MonoBehaviour {
-	Vector3 position;
-
+public class StabilizeSprite : MonoBehaviour {
+	
 	// Use this for initialization
 	void Start () {
-		position = transform.position;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.rotation = Quaternion.identity;
-		transform.position = transform.parent.position + position;
 	}
 }
