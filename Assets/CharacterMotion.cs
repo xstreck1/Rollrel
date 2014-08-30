4e07b763ba954ca9e3141ac7f8c7ce7dec6f48df
Adam Streck <adam.streck@gmail.com>
2014-08-30 16:02:06 +0200

    Initial commit

---
+++ Assets/CharacterMotion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMotion : MonoBehaviour {
+	private readonly Vector2 INITIAL_VELOCITY = new Vector2(10f, 0);
+	private readonly Vector2 STANDARD_PUSH = new Vector2(10f, 0);
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody2D.velocity = INITIAL_VELOCITY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody2D.AddForce(STANDARD_PUSH * Time.deltaTime);
+	}
+}
---
+++ Assets/CreateLine.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateLine : MonoBehaviour
+{
+		private Vector3 last_point = Vector3.zero;
+		private Object line;
+		private Camera camera;
+
+		// Use this for initialization
+		void Start ()
+		{
+				line = Resources.Load ("line");
+				camera = GameObject.Find ("Camera").GetComponent<Camera> ();
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				if (Input.GetMouseButtonDown (0)) {
+						last_point = camera.ScreenToWorldPoint (Input.mousePosition);
+				} else if (Input.GetMouseButtonUp (0)) {
+						last_point = Vector3.zero;
+				} else if (Input.GetMouseButton (0) && last_point != Vector3.zero) {
+						Vector3 new_point = camera.ScreenToWorldPoint (Input.mousePosition);
+						new_point.z = 0f;
+
+
+						float distance = Vector3.Distance (new_point, last_point);
+						GameObject new_line = (GameObject)Instantiate (line, last_point, Quaternion.identity);
+						Vector3 new_scale = new_line.transform.localScale * distance;
+						new_scale.y = 1f;
+						new_line.transform.localScale = new_scale;
+
+						// new_line.transform.LookAt(new_point, Vector3.left);
+						Debug.Log ("x: " + new_point.x + ", y: " + new_point.y);
+						last_point = new_point;
+				}
+		}
+
+		void OnMouseDown ()
+		{
+
+		}
+
+		void OnMouseUp ()
+		{
+
+		}
+}
