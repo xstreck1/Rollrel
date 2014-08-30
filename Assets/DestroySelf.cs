859eaa18ee9c6c47f76fa61d655876e4b06df19e
Adam Streck <adam.streck@gmail.com>
2014-08-30 18:42:45 +0200

    Now fading.

--- Assets/DestroySelf.cs
+++ Assets/DestroySelf.cs
@@ -2,18 +2,27 @@
 using System.Collections;
 
 public class DestroySelf : MonoBehaviour {
+	private float time = 5f;
+	private readonly float DISSAPEAR_TIME = 1f;
+	private SpriteRenderer renderer;
 
 	// Use this for initialization
 	void Start () {
-	
+		renderer = this.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		time -= Time.deltaTime;
+
+		if (time < 1f) {
+			renderer.color = new Color(1f,1f,1f,time);
+		} 
+		if (time < 0f) {
+			Destroy(this.gameObject);
+		}
 	}
 
 	void OnBecameInvisible() {
-		Destroy(this.gameObject);
 	}
 }
